f87d2de650f0c41111ebe16175f85add4e667585
denis salvino <denis.salvino23@gmail.com>
2021-05-12 04:23:04 -0500

    added hero banner

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './hero.css';
 
 const Hero = () => {
     return (
         <>
-            <div id="postOffice" className="py-5 text-center block block-1">
-                <h2>hero here</h2>
+        <div className="container-fluid mb-5 heroContainer">
+            <div id="hero" className="py-5 text-center block block-1">
             </div>
+        </div>
         </>
     );
 }
