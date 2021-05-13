73bff90c12a9e7129dc4d32ca1cf42ba259701f4
denis salvino <denis.salvino23@gmail.com>
2021-05-13 01:31:03 -0500

    sorted repositories by date created descending so most recent is first seen

--- src/components/repos/repos.js
+++ src/components/repos/repos.js
@@ -12,27 +12,39 @@ const Repos = () => {
     const loadRepos = () => {
         API.getRepos()
             .then(res =>
-                setRepos(res.data)
+                setRepos(res.data) 
             )
             .catch(err => console.log(err));
     }
 
+    const sortDate = (a, b) => {
+        let dateA = a.created_at;
+        let dateB = b.created_at;
+        if (dateA < dateB) {
+            return -1
+        } if (dateA > dateB) {
+            return 1;
+        }
+        return 0;
+    }
+
+
     return (
         <div className="col-md-8">
             {repos.length ? (
-                        <div className="row row-cols-1 row-cols-md-3 g-4 mb-5">
-                        {repos.map(repo => {
-                            return (
-                                <ListItem
-                                    key={repo.id}
-                                    name={repo.name}
-                                    full_name={repo.full_name}
-                                    description={repo.description}
-                                    html_url={repo.html_url}>
-                                </ListItem>
-                            );
-                        })}
-                    </div>
+                <div className="row row-cols-1 row-cols-md-3 g-4 mb-5">
+                    {repos.sort(sortDate).reverse().map(repo => {
+                        return (
+                            <ListItem
+                                key={repo.id}
+                                name={repo.name}
+                                full_name={repo.full_name}
+                                description={repo.description}
+                                html_url={repo.html_url}>
+                            </ListItem>
+                        );
+                    })}
+                </div>
             ) : (
                 <h3>No Results to Display</h3>
             )}
